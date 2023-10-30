cd42b895d91ed7457830def69347e7c2e4a38a60
nayib <nayib.abdala@gmail.com>
2023-10-30 11:51:11 -0500

    Add google maps

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,8 +1,17 @@
-import { User } from "./User";
-import { Company } from "./Company";
+/// <reference types="@types/google.maps" />
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-const person = new User();
-console.log(person);
+// const person = new User();
+// console.log(person);
 
-const myCompany = new Company();
-console.log(myCompany)
+// const myCompany = new Company();
+// console.log(myCompany)
+
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom:1,
+  center: {
+    lat: 0,
+    lng: 0,
+  }
+});
