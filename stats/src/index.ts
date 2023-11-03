56b9bdd62e00d38cdbaaeb00e32f13d04f0cafca
nayib <nayib.abdala@gmail.com>
2023-11-03 11:24:36 -0500

    Add first read csv file data and eport implementation

---
+++ stats/src/index.ts
@@ -0,0 +1,21 @@
+import fs, { readFileSync } from 'fs';
+
+const matches = readFileSync( 'football.csv' , {
+  encoding: 'utf-8'
+})
+.split('\n')
+.map((row: string): string[] => {
+  return row.split(',');
+});
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++
+  }
+};
+
+console.log(`Man United has won ${manUnitedWins} times`)
