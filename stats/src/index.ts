c3feb7bf65b5dc6573fea54fc939fabd3f04a95d
nayib <nayib.abdala@gmail.com>
2023-11-07 15:50:36 -0500

    refactor csv file reader

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,4 +1,5 @@
 import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
 
 const reader = new CsvFileReader('football.csv');
 reader.read();
@@ -6,12 +7,6 @@ reader.read();
 console.log(reader.data);
 
 let manUnitedWins = 0;
-
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw =  'D'
-}
 
 for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
